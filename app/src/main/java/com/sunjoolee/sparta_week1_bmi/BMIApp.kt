33cd82a1acbf940cc741a3cdf76f46de75e604c8
sunjoolee <sheisthepoem@naver.com>
2024-04-23 23:35:37 +0900

    [Rename] Rename navigation route string

--- app/src/main/java/com/sunjoolee/sparta_week1_bmi/BMIApp.kt
+++ app/src/main/java/com/sunjoolee/sparta_week1_bmi/BMIApp.kt
@@ -1,11 +1,11 @@
 package com.sunjoolee.sparta_week1_bmi
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.sunjoolee.sparta_week1_bmi.first.FirstScreen
+import com.sunjoolee.sparta_week1_bmi.result.ResultScreen
 
 @Composable
 fun BMIApp (mainViewModel:MainViewModel){
@@ -13,19 +13,13 @@ fun BMIApp (mainViewModel:MainViewModel){
     
     NavHost(
         navController = navController,
-        startDestination = "main_screen"
+        startDestination = "first_screen"
     ){
-        composable("main_screen"){
-            MainScreen(navController = navController, mainViewModel = mainViewModel)
+        composable("first_screen"){
+            FirstScreen(navController = navController, mainViewModel = mainViewModel)
         }
         composable("result_screen"){
             ResultScreen(navController = navController, mainViewModel = mainViewModel)
         }
     }
-
-//    val showResultScreen = rememberSaveable {
-//        mutableStateOf(mainViewModel.showResultScreen.value!!)
-//    }
-//    if(showResultScreen.value) ResultScreen(mainViewModel = mainViewModel)
-//    else MainScreen(mainViewModel = mainViewModel)
 }
