6abb44332f801e63c95736c64adbfc9d1da7452b
sunjoolee <sheisthepoem@naver.com>
2024-04-24 20:25:51 +0900

    [Rename] Rename MainActivity to BMIActivity

--- app/src/main/java/com/sunjoolee/sparta_week1_bmi/BMIActivity.kt
+++ app/src/main/java/com/sunjoolee/sparta_week1_bmi/BMIActivity.kt
@@ -1,17 +1,14 @@
 package com.sunjoolee.sparta_week1_bmi
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    private val mainViewModel by viewModels<MainViewModel>()
+class BMIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BMIApp(mainViewModel = mainViewModel)
+            BMINavGraph()
         }
     }
 }
