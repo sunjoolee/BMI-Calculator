67c4a1ebf5a955a8adf9de071feb89b643a55b7d
sunjoolee <sheisthepoem@naver.com>
2024-04-26 00:54:50 +0900

    [Style] Rename drawable resources

--- app/src/main/java/com/sunjoolee/sparta_week1_bmi/result/ResultScreenStateHolder.kt
+++ app/src/main/java/com/sunjoolee/sparta_week1_bmi/result/ResultScreenStateHolder.kt
@@ -28,37 +28,37 @@ class ResultScreenStateHolder(height: Float, weight: Float) {
             in 0.0..18.4 -> {
                 bmiInfoId = R.string.result_level_under
                 bmiInfoTextColorId = R.color.level1_text_color
-                bmiEmojiId = R.drawable.bmi1
+                bmiEmojiId = R.drawable.result_bmi_under
             }
 
             in 18.5..24.9 -> {
                 bmiInfoId = R.string.result_level_normal
                 bmiInfoTextColorId = R.color.level2_text_color
-                bmiEmojiId = R.drawable.bmi2
+                bmiEmojiId = R.drawable.result_bmi_normal
             }
 
             in 25.0..29.9 -> {
                 bmiInfoId = R.string.result_level_over
                 bmiInfoTextColorId = R.color.level3_text_color
-                bmiEmojiId = R.drawable.bmi3
+                bmiEmojiId = R.drawable.result_bmi_over
             }
 
             in 30.0..39.9 -> {
                 bmiInfoId = R.string.result_level_obese
                 bmiInfoTextColorId = R.color.level4_text_color
-                bmiEmojiId = R.drawable.bmi4
+                bmiEmojiId = R.drawable.result_bmi_obese
             }
 
             in 40.0F .. Float.MAX_VALUE -> {
                 bmiInfoId = R.string.result_level_extreme
                 bmiInfoTextColorId = R.color.level5_text_color
-                bmiEmojiId = R.drawable.bmi5
+                bmiEmojiId = R.drawable.result_bmi_extreme
             }
 
             else -> { // not reached
                 bmiInfoId = R.string.result_level_wrong
                 bmiInfoTextColorId = R.color.level5_text_color
-                bmiEmojiId = R.drawable.bmi5
+                bmiEmojiId = R.drawable.result_bmi_extreme
             }
         }
     }
